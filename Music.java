082aa5d606639b7ecbb13f35fd4da2730ba3bedc
KiraLittle <152565167+KiraLittle@users.noreply.github.com>
2024-11-20 15:39:57 -0500

    Music and Sound feature completed
    Music and Sound feature completed

---
+++ Music.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+//import (name of file)
+/**
+ * Write a description of class Music here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Music extends Actor
+{
+    private GreenfootImage pause =new GreenfootImage("Pause");
+    private GreenfootImage play = new GreenfootImage("Play");
+    boolean isPressed;
+    public void button(){
+        MouseInfo mouse = Greenfoot.getMouseInfo();
+        isPressed = false;
+    }
+    /**
+     * music options:
+     * 509126__freesamplemelodies__trap-melody.wav
+     * 580835__northboi__shes-gone-rb-loop-126bpm-by-northboi.wav
+     * 664888__yellowtree__rb-clean-guitar.wav
+     */
+    public void act()
+    {
+        Greenfoot.playSound("664888__yellowtree__rb-clean-guitar.wav");
+        setImage(play);
+        if(!isPressed && Greenfoot.mouseClicked(play)){
+            setImage("Pause");
+        }else if(isPressed && Greenfoot.mouseClicked(pause)){
+            
+        }
+        
+    }
+}
---
+++ Sounds.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Sounds here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Sounds extends Actor
+{
+    /**
+     * Sound Options:
+     * applause_y.wav
+     * boing_x.wav
+     * boo.wav
+     * bubbling1.wav
+     * bugs_adios.wav
+     * mouse-clicks-mouse-double-clicks-26939.mp3
+     */
+    public void act()
+    {
+        Greenfoot.playSound("mouse-clicks-mouse-double-clicks-26939.mp3");   
+    }
+}
