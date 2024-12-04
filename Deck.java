3ba9d910772082a90a8eb5b5aafe473af1f42048
KiraLittle <152565167+KiraLittle@users.noreply.github.com>
2024-12-03 20:20:38 -0500

    deck
    the deck is visible and placed accordingly.

---
+++ Deck.java
@@ -0,0 +1,39 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.List;
+
+/**
+ * Write a description of class Deck here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Deck extends Actor
+{
+    public String cardDeck[];
+    private String imageFileName;
+    public Deck(){
+        cardDeck = new String[]{"butterfly2","butterfly3", "butterfly4", "butterfly5", "butterfly6",
+        "butterfly7", "butterfly8", "butterfly9", "butterfly10", "butterflyace",
+        "butterflyjack", "butterflyqueen", "butterflyking", "bird2", "bird3",
+        "bird4", "bird5", "bird6", "bird7", "bird8", "bird9", "bird10",
+        "birdace", "birdjack", "birdqueen", "Kingbird", "gate2", "gate3",
+        "gate4", "gate5", "gate6", "gate7", "gate8", "gate9", "gate10",
+        "gateace", "gatejack", "gatequeen", "gateking", "key2", "key3",
+        "key4", "key5", "key6", "key7", "key8", "key9", "key10",
+        "keyace", "keyjack", "keyqueen", "keyking"
+        };
+         
+        
+    }
+    public void setImage(){
+        
+    }
+    /**
+     * Act - do whatever the Deck wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        
+    }
+}
